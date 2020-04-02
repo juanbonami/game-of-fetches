49d22ae0d0ca22b41a9ae7db85c1378329cc72b0
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-01 23:07:09 -0500

    first api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Where was Margaery Tyrell born?
+// URL: http://anapioficeandfire.com/api/characters/16
 
-function App() {
-  return (
-    <div className="App">
+// What region is House Targaryen in?
+// URL: http://www.anapioficeandfire.com/api/houses/378
+
+// What's the coat of arms of House Lannister?
+// URL: http://www.anapioficeandfire.com/api/houses/229
+
+// What is the second seat of House Baratheon?
+// URL: http://www.anapioficeandfire.com/api/houses/17
+
+// What is Robert Baratheon's second alias?
+// URL: http://www.anapioficeandfire.com/api/characters/901
+
+// What's the name of the founder of House Stark? (You have to chain fetch requests!)
+// URL: http://www.anapioficeandfire.com/api/houses/362
+
+// What are the titles of Catelyn Stark's three POV books? (Look into Promise.all to request these simultaniously)
+// URL: http://www.anapioficeandfire.com/api/characters/232
+
+import React, { Component } from 'react'
+import './App.css';
+import axios from 'axios'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: ''
+    }
+  }
+
+  componentDidMount() {
+    let one = "http://anapioficeandfire.com/api/characters/16";
+
+    const requestOne = axios.get(one);
+    requestOne.then(response => {
+      const retrieve = response.data.born
+      console.log(retrieve)
+
+      this.setState({
+      isLoaded: true,
+      data: retrieve 
+    })
       
-    </div>
-  );
+    }).catch(error => {
+      console.err(error)
+    })
+
+    
+
 }
 
-export default App;
+
+  render() {
+    return (
+      <div>
+        <h1> {this.state.data} </h1>
+        {/* <ul>
+          {this.state.data.map((recipe, id) => <li key={id}> {recipe.born} </li>)}
+        </ul> */}
+      </div>
+    )
+  }
+}
