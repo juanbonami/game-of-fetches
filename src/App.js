e2102d9a1751e2359cca4f113dd1d163b6ba1c9a
Student_M17 <student_m17@ps-dal-m17.home>
2020-04-02 00:04:53 -0500

    axios.all .then .spread .catch

--- src/App.js
+++ src/App.js
@@ -29,26 +29,58 @@ export default class App extends Component {
 
     this.state = {
       isLoaded: false,
-      data: ''
+      data: []
     }
   }
 
   componentDidMount() {
     let one = "http://anapioficeandfire.com/api/characters/16";
+    let two = "http://www.anapioficeandfire.com/api/houses/378";
+    let three = "http://www.anapioficeandfire.com/api/houses/229";
+    let four = "http://www.anapioficeandfire.com/api/houses/17";
+    let five = "http://www.anapioficeandfire.com/api/characters/901";
+    let six = "http://www.anapioficeandfire.com/api/houses/362";
+    let seven = "http://www.anapioficeandfire.com/api/characters/232";
 
     const requestOne = axios.get(one);
-    requestOne.then(response => {
-      const retrieve = response.data.born
-      console.log(retrieve)
+    const requestTwo = axios.get(two);
+    const requestThree = axios.get(three);
+    const requestFour = axios.get(four);
+    const requestFive = axios.get(five);
+    const requestSix = axios.get(six);
+    const requestSeven = axios.get(seven);
 
-      this.setState({
-      isLoaded: true,
-      data: retrieve 
-    })
+    axios.all([requestOne, requestTwo, requestThree, requestFour, requestFive, requestSix, requestSeven])
+    .then(axios.spread((...responses) => {
       
-    }).catch(error => {
-      console.err(error)
+      const responseOne = responses[0]
+      const responseTwo = responses[1]
+      const responseThree = responses[2]
+      const responseFour = responses[3]
+      const responseFive = responses[4]
+      const responseSix = responses[5]
+      const responseSeven = responses[6]
+      console.log(responses)
+      // this.setState({
+      //   data: 
+      // })
+
+    })).catch(errors => {
+      console.log('Error fetching data')
     })
+
+    // requestOne.then(response => {
+    //   const retrieve = response.data.born
+    //   console.log(retrieve)
+
+    //   this.setState({
+    //   isLoaded: true,
+    //   data: retrieve 
+    // })
+      
+    // }).catch(error => {
+    //   console.err(error)
+    // })
 
     
 
